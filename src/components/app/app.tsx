fbff6639d5ff0f68f0fa7df795a4ee9f4b00dfb8
Chingis Choybsonov <avatarius93@gmail.com>
2024-06-10 18:26:31 +0300

    конструктор бургера

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,72 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
+import { Feed } from '@pages';
+import { Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from '../../services/store';
+import { fetchBurgers } from '../../slices/burgerSlice';
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
-);
+const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchBurgers());
+  }, []);
+
+  return (
+    <div className={styles.app}>
+      {/* <AppHeader />
+    <ConstructorPage /> */}
+      <Routes>
+        <Route path='/' element={<AppHeader />}>
+          <Route index element={<ConstructorPage />} />
+          {/* <Route path='/feed' element={<Feed />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/reset-password' element={<ResetPassword />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/profile/orders' element={<ProfileOrders />} />
+          <Route path='*' element={<NotFound404 />} />
+          <Route
+            path='/feed/:number'
+            element={
+              <Modal title='test' onClose={() => console.log('clsoe')}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route
+            path='/ingredients/:id'
+            element={
+              <Modal title='test' onClose={() => console.log('clsoe')}>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+          <Route
+            path='/profile/orders/:number'
+            element={
+              <Modal title='test' onClose={() => console.log('clsoe')}>
+                <IngredientDetails />
+              </Modal>
+            }
+          /> */}
+        </Route>
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/slices/burgerSlice.ts
@@ -0,0 +1,73 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { getIngredientsApi } from '@api';
+import { TConstructorIngredient, TIngredient } from '@utils-types';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { v4 as uuidv4 } from 'uuid';
+
+interface IConstructorItems {
+  bun: TIngredient | null;
+  ingredients: TConstructorIngredient[];
+}
+
+export interface IngredientListState {
+  isLoading: boolean;
+  ingredients: TIngredient[];
+  constructorItems: IConstructorItems;
+}
+
+const initialState: IngredientListState = {
+  isLoading: false,
+  ingredients: [],
+  constructorItems: { bun: null, ingredients: [] }
+};
+const fetchBurgers = createAsyncThunk('burgers/fetch', async () =>
+  getIngredientsApi()
+);
+
+const burgerSlice = createSlice({
+  name: 'burgers',
+  initialState,
+  reducers: {
+    addToConstructor: (state, action) => {
+      action.payload.type === 'bun'
+        ? (state.constructorItems.bun = action.payload)
+        : state.constructorItems.ingredients.push({
+            ...action.payload,
+            id: uuidv4()
+          });
+    },
+    removeFromConstructor: (state, action) => {
+      state.constructorItems.ingredients =
+        state.constructorItems.ingredients.filter(
+          (item) => item.id !== action.payload.id
+        );
+    }
+  },
+  selectors: {
+    selectIngredients: (state) => state.ingredients,
+    selectConstructorItems: (state) => state.constructorItems
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchBurgers.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchBurgers.fulfilled, (state, action) => {
+        state.ingredients = action.payload;
+      })
+      .addCase(fetchBurgers.rejected, (state, action) => {});
+  }
+});
+
+const burgerReducer = burgerSlice.reducer;
+const { selectIngredients, selectConstructorItems } = burgerSlice.selectors;
+const { addToConstructor, removeFromConstructor } = burgerSlice.actions;
+
+export {
+  burgerReducer,
+  selectIngredients,
+  selectConstructorItems,
+  addToConstructor,
+  removeFromConstructor,
+  fetchBurgers
+};
